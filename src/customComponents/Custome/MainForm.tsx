1c8b808f346339f9536139e3b2f1806fef08dd16
AdanTouchPoint <adan.mijangos@touchpointmarketing.mx>
2024-10-22 15:42:14 -0600

    style & refactorize mainform component done!

--- src/customComponents/Custome/MainForm.tsx
+++ src/customComponents/Custome/MainForm.tsx
@@ -1,4 +1,4 @@
-import React, {FormEvent,useEffect} from 'react';
+import React, {FormEvent,useEffect, useState} from 'react';
 import "./sb.css"
 import { useAuth } from 'payload/components/utilities';
 import { MainFormProps, ProjectData } from '../interfaces';
@@ -6,13 +6,12 @@ import { postCampaignData } from '../../lib/requestsAPI';
 import sbImage from '../assets/SBImage.png'
 import pdImage from '../assets/PDImage.png'
 import apImage from '../assets/APImage.png'
-
-
+type ActiveSection = 'campaign-type' | 'data-form';
 const baseClass = 'after-dashboard';
 const MainForm: React.FC<MainFormProps> = ({setErr,err,setActiveForm,projectData,setProjectData}) => {
 const  user = useAuth();
 const userId = user.user.id
-
+const [activeSection,setActiveSection] = useState<ActiveSection>('campaign-type')
 const verifyInputs = ( projectData ) => {
   if (
     !projectData?.campaignType?.trim() || 
@@ -28,7 +27,7 @@ const verifyInputs = ( projectData ) => {
   const click = async () => {
   try {
   const validate = await verifyInputs(projectData)
-  //añadir request para guardar en BD
+  // guardar en BD
     const prepareData= { projectData, clientId: userId}
     const data = await postCampaignData(prepareData)
     const id = await  data.data.id
@@ -38,73 +37,29 @@ const verifyInputs = ( projectData ) => {
     }
     setProjectData(payload)
     setActiveForm(projectData.campaignType)
-    
 } catch (error) {
     throw new Error (error)
   }
+}
+const back =() => {
+setActiveSection('campaign-type')
 }
 const handleOnChange = (event: FormEvent<HTMLInputElement>)  => {
   const info : ProjectData = {
     ...projectData,
     [(event.target as HTMLInputElement).name]: (event.target as HTMLInputElement).value
   }
-  return setProjectData(info)
+  setProjectData(info)
+  setActiveSection('data-form')
 } 
-	return (
-<div className={baseClass}>
-  <div className="gutter--left gutter--right collection-list__wrap form-container">
-    <br />
-    <p className="section-title">Choose Campaign Type:</p>
-    <div className="campaign-options">
-      {/* Tarjeta 1 */}
-      <div className="campaign-card">
-        <input
-          onChange={handleOnChange}
-          name="campaignType"
-          value="SB"
-          type="checkbox"
-          id="campaign-sb"
-        />
-        <label htmlFor="campaign-sb">
-          <img src={sbImage} alt="Option 1 Image" />
-          <h3>Option 1</h3>
-          <p>Breve descripción de la Opción 1.</p>
-        </label>
-      </div>
-      {/* Tarjeta 2 */}
-      <div className="campaign-card">
-        <input
-          onChange={handleOnChange}
-          name="campaignType"
-          value="PD"
-          type="checkbox"
-          id="campaign-pd"
-        />
-        <label htmlFor="campaign-pd">
-          <img src={pdImage} alt="Option 2 Image" />
-          <h3>Option 2</h3>
-          <p>Breve descripción de la Opción 2.</p>
-        </label>
-      </div>
-      {/* Tarjeta 3 */}
-      <div className="campaign-card">
-        <input
-          onChange={handleOnChange}
-          name="campaignType"
-          value="AP"
-          type="checkbox"
-          id="campaign-ap"
-        />
-        <label htmlFor="campaign-ap">
-          <img src={apImage} alt="Option 3 Image" />
-          <h3>Option 3</h3>
-          <p>Breve descripción de la Opción 3.</p>
-        </label>
-      </div>
-    </div>
-
-    {/* Resto del formulario */}
-    <p className="section-title">Campaign Info</p>
+const renderForm = () => (
+<>
+{/* Resto del formulario */}
+<div className='baseClass'>
+  <div className='form-container'> 
+  <div className="form-card">
+<div className='form-section'> 
+<p className="section-title">Campaign Info</p>
     <div className="form-group">
       <label>Name:</label>
       <input name="repo" onChange={handleOnChange} type="text" />
@@ -117,11 +72,83 @@ const handleOnChange = (event: FormEvent<HTMLInputElement>)  => {
       <label>Title:</label>
       <input name="title" onChange={handleOnChange} type="text" />
     </div>
+    <button onClick={back} className="submit-button">
+      Back
+    </button>
     <button onClick={click} className="submit-button">
       Next
     </button>
+</div>
   </div>
 </div>
+
+</div>
+    </>
+    
+)
+const renderCampaignType = () => (
+  <div className='ctype'>
+     <br />
+    <p className="section-title">Choose Campaign Type:</p>
+    <div className="campaign-options">
+      {/* Tarjeta 1 */}
+      <div className="campaign-card">
+        <input
+          onClick={handleOnChange}
+          name="campaignType"
+          value="SB"
+          type="radio"
+          id="campaign-sb"
+          hidden
+        />
+        <label htmlFor="campaign-sb">
+          <img src={sbImage} alt="Option 1 Image" />
+          <h3>Submission Builder</h3>
+          <p>Breve descripción de la Opción 1.</p>
+        </label>
+      </div>
+      {/* Tarjeta 2 */}
+      <div className="campaign-card">
+        <input
+          onClick={handleOnChange}
+          name="campaignType"
+          value="PD"
+          type="radio"
+          id="campaign-pd"
+          hidden
+        />
+        <label htmlFor="campaign-pd">
+          <img src={pdImage} alt="Option 2 Image" />
+          <h3>Politicall Direct</h3>
+          <p>Breve descripción de la Opción 2.</p>
+        </label>
+      </div>
+      {/* Tarjeta 3 */}
+      <div className="campaign-card">
+        <input
+          onClick={handleOnChange}
+          name="campaignType"
+          value="AP"
+          type="button"
+          id="campaign-ap"
+          hidden
+        />
+        <label htmlFor="campaign-ap">
+          <img src={apImage} alt="Option 3 Image" />
+          <h3>Alert the Press</h3>
+          <p>Breve descripción de la Opción 3.</p>
+        </label>
+      </div>
+    </div>
+  </div>
+)
+	return (
+
+<>
+{activeSection === 'campaign-type' && renderCampaignType()}
+{activeSection === 'data-form' && renderForm()}    
+</>
+
 
 
   

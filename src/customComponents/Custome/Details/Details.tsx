4ee3decce12cb3fbb4d8c838c46fb5debe6822a4
AdanTouchPoint <adan.mijangos@touchpointmarketing.mx>
2025-07-18 12:37:58 -0600

    added export report function

--- src/customComponents/Custome/Details/Details.tsx
+++ src/customComponents/Custome/Details/Details.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./Details.module.css";
-import { DetailsProps  } from "../../interfaces";
+import { DetailsProps } from "../../interfaces";
 import DeleteModal from "./DeleteModal";
 import PauseModal from "./PauseModal";
+import { exportComponentToPDF } from "../../../lib/misc"; // Asegúrate de que esta función esté definida correctamente
 
-const CampaignDetails: React.FC<DetailsProps> = ({ projectData, setActiveView, setProjectData }) => {
+const CampaignDetails: React.FC<DetailsProps> = ({
+  projectData,
+  setActiveView,
+  setProjectData,
+}) => {
   const [showDeleteModal, setShowDeleteModal] = React.useState(false);
   const [showPauseModal, setShowPauseModal] = React.useState(false);
+  const contentRef = useRef<HTMLDivElement>(null);
   const handleDotColor = (status: string) => {
     switch (status) {
       case "active":
@@ -30,20 +36,15 @@ const CampaignDetails: React.FC<DetailsProps> = ({ projectData, setActiveView, s
     type: projectData.campaignType || "Digital",
     startDate: projectData.startDate || "2023-01-01",
     endDate: projectData.endDate || "2023-12-31",
-    objective: projectData.description || "Aumentar la conciencia sobre el reciclaje",
+    objective:
+      projectData.description || "Aumentar la conciencia sobre el reciclaje",
     manager: "Juan Pérez",
     visits: 20000,
-    ctr: "5%",
-    bounceRate: "30%",
-    avgTime: "2 min 30 sec",
-    progress: 75,
     status: projectData.status || "offline",
     startDateFull: "1 de enero de 2023",
     endDateFull: "31 de diciembre de 2023",
     daysElapsed: 150,
     daysRemaining: 215,
-    ownerInitial: "JP",
-    owner: "Juan Pérez",
   };
   const back = () => {
     setActiveView("all");
@@ -54,196 +55,197 @@ const CampaignDetails: React.FC<DetailsProps> = ({ projectData, setActiveView, s
         return "Submission Builder Campaign";
       case "PD":
         return "Politicall Direct Campaign";
-    case "AP":
+      case "AP":
         return "Alert the Press Campaign";
       default:
         return "Unknown Campaign Type";
     }
   };
   return (
-
-      <div className={styles.contentAll}>
-        {showDeleteModal && (
-          <DeleteModal
-            onClose={() => setShowDeleteModal(false)}
-            projectData={projectData}
-            setProjectData={setProjectData}
-          />
-        )}
-        {showPauseModal && (
-          <PauseModal
-            onClose={() => setShowPauseModal(false)}
-            projectData={projectData}
-            setProjectData={setProjectData}
-          />
-        )}
-        <div className={styles.campaignContainer}>
-          <div className={styles.campaignHeader}>
-            <div className={styles.campaignTitleContainer}>
-              <div className={styles.campaignId}>ID: {campaign.id}</div>
-              <h1 className={styles.campaignTitle}>{campaign.title}</h1>
-              <p className={styles.campaignSubtitle}>{campaign.objective}</p>
-              <div className={styles.headerStats}>
-                <div className={styles.headerStat}>
-                  <span className={styles.statValue}>{campaign.leads}</span>
-                  <span className={styles.statLabel}>Leads</span>
-                </div>
-                <div className={styles.headerStat}>
-                  <span className={styles.statValue}>
-                    {campaign.visits}
-                  </span>
-                  <span className={styles.statLabel}>Email sended </span>
-                </div>
-
+    <div className={styles.contentAll}>
+      {showDeleteModal && (
+        <DeleteModal
+          onClose={() => setShowDeleteModal(false)}
+          projectData={projectData}
+          setProjectData={setProjectData}
+        />
+      )}
+      {showPauseModal && (
+        <PauseModal
+          onClose={() => setShowPauseModal(false)}
+          projectData={projectData}
+          setProjectData={setProjectData}
+        />
+      )}
+      <div ref={contentRef} className={styles.campaignContainer}>
+        <div className={styles.campaignHeader}>
+          <div className={styles.campaignTitleContainer}>
+            <div className={styles.campaignId}>ID: {campaign.id}</div>
+            <h1 className={styles.campaignTitle}>{campaign.title}</h1>
+            <p className={styles.campaignSubtitle}>{campaign.objective}</p>
+            <div className={styles.headerStats}>
+              <div className={styles.headerStat}>
+                <span className={styles.statValue}>{campaign.leads}</span>
+                <span className={styles.statLabel}>Leads</span>
+              </div>
+              <div className={styles.headerStat}>
+                <span className={styles.statValue}>{campaign.visits}</span>
+                <span className={styles.statLabel}>Email sended </span>
               </div>
             </div>
           </div>
-
-          <div className={styles.campaignBody}>
-            <div className={styles.mainContent}>
-              <div className={styles.section}>
-                <h2 className={styles.sectionTitle}>
-                  <i className="fas fa-info-circle"></i>
-                    General Information  
-                </h2>
-                <div className={styles.infoGrid}>
-                  <div className={styles.infoItem}>
-                    <span className={styles.infoLabel}>
-                      <i className="fas fa-link"></i>
-                      URL Campaign
-                    </span>
-                    { campaign.url === "NO URL" ? (
-                      <span className={styles.infoValue}>No URL provided</span>
-                    ) : (
-                      <a
-                        href={campaign.url}
-                        className={`${styles.infoValue} ${styles.urlValue}`}
-                        target="_blank"
-                      >
-                        {campaign.url}
-                      </a>
-                    )}
-                  </div>
-                  <div className={styles.infoItem}>
-                    <span className={styles.infoLabel}>
-                      <i className="fas fa-tags"></i>
-                      Campaign Type
-                    </span>
-                    <span className={styles.infoValue}>{formatTypesNames(campaign.type)}</span>
-                  </div>
-                  <div className={styles.infoItem}>
-                    <span className={styles.infoLabel}>
-                      <i className="fas fa-calendar-alt"></i>
-                      Start Date
-                    </span>
-                    <span className={styles.infoValue}>
-                      {campaign.startDate}
-                    </span>
-                  </div>
-                  <div className={styles.infoItem}>
-                    <span className={styles.infoLabel}>
-                      <i className="fas fa-calendar-check"></i>
-                      End Date
-                    </span>
-                    <span className={styles.infoValue}>{campaign.endDate}</span>
-                  </div>
-                  <div className={styles.infoItem}>
-                    <span className={styles.infoLabel}>
-                      <i className="fas fa-bullseye"></i>
-                      Objective
-                    </span>
-                    <span className={styles.infoValue}>
-                      {campaign.objective}
-                    </span>
-                  </div>
-                  <div className={styles.infoItem}>
-                    <span className={styles.infoLabel}>
-                      <i className="fas fa-user-tie"></i>
-                      Responsible
-                    </span>
-                    <span className={styles.infoValue}>{campaign.manager}</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className={styles.sidebar}>
-              <div className={styles.statusCard}>
-                <h2 className={styles.statusTitle}>
-                  <i className="fas fa-clipboard-check"></i>
-                  Current Status
-                </h2>
-                <div className={styles.statusIndicator}>
-                  <div className={handleDotColor(campaign.status)}></div>
-                  <div className={styles.indicatorText}>{campaign.status }</div>
-                </div>
-                <h3
-                  className={styles.statusTitle}
-                  style={{ fontSize: "1.1rem", marginTop: "25px" }}
-                >
-                  <i className="fas fa-calendar-day"></i>
-                  Timeline
-                </h3>
-                <div className={styles.datesContainer}>
-                  <div className={styles.dateItem}>
-                    <span className={styles.dateLabel}>Start Date:</span>
-                    <span className={styles.dateValue}>
-                      {campaign.startDateFull}
-                    </span>
-                  </div>
-                  <div className={styles.dateItem}>
-                    <span className={styles.dateLabel}>
-                      End Date:
-                    </span>
-                    <span className={styles.dateValue}>
-                      {campaign.endDateFull}
-                    </span>
-                  </div>
-                  <div className={styles.dateItem}>
-                    <span className={styles.dateLabel}>
-                      Days Elapsed:
-                    </span>
-                    <span className={styles.dateValue}>
-                      {campaign.daysElapsed} days
-                    </span>
-                  </div>
-                  <div className={styles.dateItem}>
-                    <span className={styles.dateLabel}>Days Remaining:</span>
-                    <span className={styles.dateValue}>
-                      {campaign.daysRemaining} days
-                    </span>
-                  </div>
-                </div>
-              </div>
-
-              <div className={styles.actionsCard}>
-                <h2 className={styles.actionTitle}>
-                  <i className="fas fa-cog"></i>
-                  Actions
-                </h2>
-                <div className={styles.actionButtons}>
-                  <button className={styles.actionBtn}>
-                    <i className="fas fa-edit"></i>
-                    Export Data
-                  </button>
-                  <button disabled={campaign.url === "NO URL"? true : false}  onClick={() => setShowPauseModal(true)} className={styles.actionBtn}>
-                    {campaign.status === "paused" ? "Resume Campaign" : "Pause Campaign"}
-                  </button>
-                  <button onClick={() => setShowDeleteModal(true)} className={styles.actionBtn}>
-                    <i className="fas fa-file-export"></i>
-                    Delete Campaign
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        <button onClick={back}>
-          Back to Campaigns
-        </button>
         </div>
 
-      </div>
+        <div className={styles.campaignBody}>
+          <div className={styles.mainContent}>
+            <div className={styles.section}>
+              <h2 className={styles.sectionTitle}>
+                <i className="fas fa-info-circle"></i>
+                General Information
+              </h2>
+              <div className={styles.infoGrid}>
+                <div className={styles.infoItem}>
+                  <span className={styles.infoLabel}>
+                    <i className="fas fa-link"></i>
+                    URL Campaign
+                  </span>
+                  {campaign.url === "NO URL" ? (
+                    <span className={styles.infoValue}>No URL provided</span>
+                  ) : (
+                    <a
+                      href={campaign.url}
+                      className={`${styles.infoValue} ${styles.urlValue}`}
+                      target="_blank"
+                    >
+                      {campaign.url}
+                    </a>
+                  )}
+                </div>
+                <div className={styles.infoItem}>
+                  <span className={styles.infoLabel}>
+                    <i className="fas fa-tags"></i>
+                    Campaign Type
+                  </span>
+                  <span className={styles.infoValue}>
+                    {formatTypesNames(campaign.type)}
+                  </span>
+                </div>
+                <div className={styles.infoItem}>
+                  <span className={styles.infoLabel}>
+                    <i className="fas fa-calendar-alt"></i>
+                    Start Date
+                  </span>
+                  <span className={styles.infoValue}>{campaign.startDate}</span>
+                </div>
+                <div className={styles.infoItem}>
+                  <span className={styles.infoLabel}>
+                    <i className="fas fa-calendar-check"></i>
+                    End Date
+                  </span>
+                  <span className={styles.infoValue}>{campaign.endDate}</span>
+                </div>
+                <div className={styles.infoItem}>
+                  <span className={styles.infoLabel}>
+                    <i className="fas fa-bullseye"></i>
+                    Objective
+                  </span>
+                  <span className={styles.infoValue}>{campaign.objective}</span>
+                </div>
+                <div className={styles.infoItem}>
+                  <span className={styles.infoLabel}>
+                    <i className="fas fa-user-tie"></i>
+                    Responsible
+                  </span>
+                  <span className={styles.infoValue}>{campaign.manager}</span>
+                </div>
+              </div>
+            </div>
+          </div>
 
+          <div className={styles.sidebar}>
+            <div className={styles.statusCard}>
+              <h2 className={styles.statusTitle}>
+                <i className="fas fa-clipboard-check"></i>
+                Current Status
+              </h2>
+              <div className={styles.statusIndicator}>
+                <div className={handleDotColor(campaign.status)}></div>
+                <div className={styles.indicatorText}>{campaign.status}</div>
+              </div>
+              <h3
+                className={styles.statusTitle}
+                style={{ fontSize: "1.1rem", marginTop: "25px" }}
+              >
+                <i className="fas fa-calendar-day"></i>
+                Timeline
+              </h3>
+              <div className={styles.datesContainer}>
+                <div className={styles.dateItem}>
+                  <span className={styles.dateLabel}>Start Date:</span>
+                  <span className={styles.dateValue}>
+                    {campaign.startDateFull}
+                  </span>
+                </div>
+                <div className={styles.dateItem}>
+                  <span className={styles.dateLabel}>End Date:</span>
+                  <span className={styles.dateValue}>
+                    {campaign.endDateFull}
+                  </span>
+                </div>
+                <div className={styles.dateItem}>
+                  <span className={styles.dateLabel}>Days Elapsed:</span>
+                  <span className={styles.dateValue}>
+                    {campaign.daysElapsed} days
+                  </span>
+                </div>
+                <div className={styles.dateItem}>
+                  <span className={styles.dateLabel}>Days Remaining:</span>
+                  <span className={styles.dateValue}>
+                    {campaign.daysRemaining} days
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div data-html2canvas-ignore className={styles.actionsCard}>
+              <h2 className={styles.actionTitle}>
+                <i className="fas fa-cog"></i>
+                Actions
+              </h2>
+              <div className={styles.actionButtons}>
+                <button
+                  onClick={() =>
+                    exportComponentToPDF(contentRef.current, "report.pdf")
+                  }
+                  className={styles.actionBtn}
+                >
+                  <i className="fas fa-edit"></i>
+                  Export Data
+                </button>
+                <button
+                  disabled={campaign.url === "NO URL" ? true : false}
+                  onClick={() => setShowPauseModal(true)}
+                  className={styles.actionBtn}
+                >
+                  {campaign.status === "paused"
+                    ? "Resume Campaign"
+                    : "Pause Campaign"}
+                </button>
+                <button
+                  onClick={() => setShowDeleteModal(true)}
+                  className={styles.actionBtn}
+                >
+                  <i className="fas fa-file-export"></i>
+                  Delete Campaign
+                </button>
+              </div>
+            </div>
+            <button data-html2canvas-ignore onClick={back}>
+              Back to Campaigns
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/lib/misc.ts
+++ src/lib/misc.ts
@@ -1,3 +1,54 @@
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
+
+// crea una funcion para exportar en pdf un screenshoot del componente
+export async function exportComponentToPDF(element: HTMLElement, fileName ) {
+    
+   const options = {
+      scale: 2, // Calidad aumentada
+      useCORS: true,
+      allowTaint: true,
+      scrollX: 0,
+      scrollY: -window.scrollY, // Asegura que capturamos desde el top
+      windowWidth: document.documentElement.scrollWidth,
+      windowHeight: document.documentElement.scrollHeight,
+      backgroundColor: '#000000ff'
+    };
+
+    html2canvas(element, options).then(canvas => {
+      // Dimensiones del contenido
+      const contentWidth = canvas.width;
+      const contentHeight = canvas.height;
+      
+      // Tamaño de página A4 en pixels (considerando 96dpi)
+      const a4Width = 794; // 210mm
+      const a4Height = 800; // 297mm
+      
+      // Calcular relación de escalado
+      const scale = Math.min(a4Width / contentWidth, a4Height / contentHeight);
+      
+      // Crear PDF
+      const pdf = new jsPDF({
+        orientation: 'portrait',
+        unit: 'px',
+        format: [a4Width, a4Height],
+        compress: true
+      });
+      
+      // Añadir imagen escalada para que quepa en una página
+      pdf.addImage(
+        canvas.toDataURL('image/png'),
+        'PNG',
+        0,
+        0,
+        contentWidth * scale,
+        contentHeight * scale
+      );
+      
+      pdf.save(fileName);
+    });
+}
+
 export function validatestring(repo) {
     return repo.toLowerCase().replace(/ /g, '_');
   }
@@ -29,7 +80,8 @@ export function validatestring(repo) {
   }
 }
 export function exportData(obj) {
-// preguntar funcionalidad
+// crea una funcion para exportar en pdf un screenshoot ddel componente
+
 }
 
 export function campaignStatus(status,id) {
